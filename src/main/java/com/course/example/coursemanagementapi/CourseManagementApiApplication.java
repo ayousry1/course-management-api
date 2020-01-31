b3f9e7c1f70cf2277306c1bbf76b2cb8ba0ed67b
ahmedyousryaast <ay1.aast@gmail.com>
2020-01-31 17:56:10 +0200

    -adding swagger

--- src/main/java/com/course/example/coursemanagementapi/CourseManagementApiApplication.java
+++ src/main/java/com/course/example/coursemanagementapi/CourseManagementApiApplication.java
@@ -2,10 +2,14 @@ package com.course.example.coursemanagementapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CourseManagementApiApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(CourseManagementApiApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementApiApplication.class, args);
+	}
+
 }
