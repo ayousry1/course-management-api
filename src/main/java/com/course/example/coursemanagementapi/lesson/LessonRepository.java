05f349fbb9a2a721f49e95312147200ffc8d8793
ahmedyousryaast <ay1.aast@gmail.com>
2020-01-31 16:16:16 +0200

    - removing unused classes

--- src/main/java/com/course/example/coursemanagementapi/lesson/LessonRepository.java
+++
@@ -1,6 +0,0 @@
-package com.course.example.coursemanagementapi.lesson;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface LessonRepository extends CrudRepository<Lesson,Integer>{
-}
